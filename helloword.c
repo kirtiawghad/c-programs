a54834dd460b47f86dab226c3bd9adaace9e4503
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-05-30 16:50:34 +0530

    2 program

--- helloword.c
+++ helloword.c
@@ -1,18 +1,15 @@
-//program 1
 # include <stdio.h>
-
 int main(int argc, char const *argv[])
 {
-    //output
-    int age = 23;
-    printf("age is age %d\n" ,age);
-   
 
-    float pi = 3.14;
-    printf("pi value is %f\n",pi);
+    int a,b;
 
-    char star = '*';
-    printf("star is %c\n", star);
+    printf("enter first number \n");
+    scanf("%d" ,& a);
 
-    return 0;
+    printf("enter second Number\n");
+    scanf("%d",& b);
+
+    printf("Result is %d",a+b);
+
 }
