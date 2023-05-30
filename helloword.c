116b4463c82ea0edfc0380295314f58b7f63187a
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-05-30 16:40:18 +0530

    c 1 program

---
+++ helloword.c
@@ -0,0 +1,18 @@
+//program 1
+# include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    //output
+    int age = 23;
+    printf("age is age %d\n" ,age);
+   
+
+    float pi = 3.14;
+    printf("pi value is %f\n",pi);
+
+    char star = '*';
+    printf("star is %c\n", star);
+
+    return 0;
+}
