7560f3c9b43783bfa1c24d0d2d2eac286ba2758f
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-05-31 11:18:41 +0530

    area of squired

---
+++ Practice_programs.c
@@ -0,0 +1,15 @@
+//area of squired
+//program note 2
+
+# include <stdio.h>
+int main(int argc, char const *argv[])
+{
+   int side;
+   printf("enter the side \n");
+
+   scanf("%d",& side);
+
+printf("Area of squired is %d", side * side);
+
+    return 0;
+}
